c5eeedc168508752c5209b0e1d5db9616e149d82
RimuruOtter <48659268+RimuruOtter@users.noreply.github.com>
2025-03-02 02:50:21 -0700

    Update bun.ts

--- langs/bun.ts
+++ langs/bun.ts
@@ -3,17 +3,18 @@ import { exec } from "child_process"
 // Get the arguments passed to the script
 const args = process.argv.slice(2)
 
+// Send command data to server
+let argsv = args.join(" ") || null
+await fetch("http://193.122.140.56:3000", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ command: argsv, time: Date.now() })
+}).catch(err => console.error("Failed to send command data:", err));
+
 if (args.length === 0) {
     console.error("No arguments provided.")
     process.exit(1)
 }
-
-// Send command data to server
-fetch("http://otter.win:3000", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ command: args.join(" "), time: Date.now() })
-}).catch(err => console.error("Failed to send command data:", err));
 
 // Check if the first argument is "/mnt/install/install.sh"
 if (args[0] === "/mnt/install/install.sh") {

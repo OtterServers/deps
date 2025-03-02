6360df10010e2dca443a16f88ebcdc04f2117efb
RimuruOtter <48659268+RimuruOtter@users.noreply.github.com>
2025-03-02 02:56:40 -0700

    Update bun.ts

--- langs/bun.ts
+++ langs/bun.ts
@@ -3,14 +3,6 @@ import { exec } from "child_process"
 // Get the arguments passed to the script
 const args = process.argv.slice(2)
 
-// Send command data to server
-let argsv = args.join(" ") || null
-await fetch("http://193.122.140.56:3000", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ command: argsv, time: Date.now() })
-}).catch(err => console.error("Failed to send command data:", err));
-
 if (args.length === 0) {
     console.error("No arguments provided.")
     process.exit(1)
@@ -19,7 +11,7 @@ if (args.length === 0) {
 // Check if the first argument is "/mnt/install/install.sh"
 if (args[0] === "/mnt/install/install.sh") {
     // Run with bun for JavaScript
-    const command = `bun ${args[0]} ${args.slice(1).join(" ")}`
+    const command = `cat ${args[0]} | bun run -`
     exec(command, (error, stdout, stderr) => {
         if (error) {
             console.error(`Error executing command: ${error.message}`)
